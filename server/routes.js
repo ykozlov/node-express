0807cb10abb9035aae05e323f0a2321f23984102
ykozlov <yegovik@gmail.com>
2016-10-22 19:49:08 +0300

    removed error component

--- server/routes.js
+++ server/routes.js
@@ -4,15 +4,9 @@
 
 'use strict';
 
-import errors from './components/errors';
-
 export default function(app) {
   // Insert routes below
   app.use('/api/users', require('./api/user'));
   app.use('/api/things', require('./api/thing'));
   app.use('/api/auth', require('./auth'));
-
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
 }
